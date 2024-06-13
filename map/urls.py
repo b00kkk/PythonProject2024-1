9ef5158bbfdbdce50615464b79731774664087ba
SeeeungJ <112918204+SeeeungJ@users.noreply.github.com>
2024-06-13 20:31:51 +0900

    Delete map directory

--- map/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-# from .views import seoul_map
-
-urlpatterns = [
-    path('', views.home, name='home'),
-    path('get_data/', views.get_data, name='get_data'),
-]
